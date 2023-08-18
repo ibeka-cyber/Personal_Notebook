4a496b443efe241b9df8a38968719a81cde5678f
Bahar <irembahar4118@gmail.com>
2023-08-19 00:41:25 +0300

    pn

---
+++ main_menu.py
@@ -0,0 +1,4 @@
+from personal_notebook import main
+if __name__=="__main__":
+    main()
+    
---
+++ show_menu.py
@@ -0,0 +1,8 @@
+def show_main_menu():
+    print("""****** NOT DEFTERİ UYGULAMASI ****** 
+             
+    1) Notları Görüntüle 
+    2) Not Ekle 
+    3) Not Sil 
+    4) Çıkış""")
+
